cf5ddd5fa51e90c4c66fcd23f6be482160ba62fb
Nguyendat0908 <Nguyendat090803@gmail.com>
2024-09-15 21:48:56 +0700

    Algorithm-LeetCode-Two_Sum

---
+++ TowSum.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = twoSum(nums, target); // Gọi phương thức twoSum và lưu kết quả
+
+        // Kiểm tra nếu kết quả khác null và in ra
+        if (result != null) {
+            for (int i : result) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("Không có kết quả");
+        }
+    }
+}
